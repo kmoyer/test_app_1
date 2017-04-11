31a7ae465d66a12c642e7a566072f389304d0a6e
Raghu Betina <raghu@firstdraft.com>
2017-04-11 01:44:02 +0000

    Generate tests resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Test resource:
+  # CREATE
+  get "/tests/new", :controller => "tests", :action => "new"
+  post "/create_test", :controller => "tests", :action => "create"
+
+  # READ
+  get "/tests", :controller => "tests", :action => "index"
+  get "/tests/:id", :controller => "tests", :action => "show"
+
+  # UPDATE
+  get "/tests/:id/edit", :controller => "tests", :action => "edit"
+  post "/update_test/:id", :controller => "tests", :action => "update"
+
+  # DELETE
+  get "/delete_test/:id", :controller => "tests", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
